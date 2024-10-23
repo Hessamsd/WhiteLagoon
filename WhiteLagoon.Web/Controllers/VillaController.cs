aa9f9757e041f7a2299a240165d6413acec3b3fa
Hessamsd <hessamsadeghi73@gmail.com>
2024-10-23 12:39:02 +0330

    Section 4 - Update in Action

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -55,6 +55,20 @@ namespace WhiteLagoon.Web.Controllers
             }
             return View(obj);
         }
+
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if(ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
     }
 }
 
