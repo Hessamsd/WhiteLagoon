a5567c95f5e563ce58c23bb9f29abc4d29a4f36c
Hessamsd <hessamsadeghi73@gmail.com>
2024-10-26 15:22:35 +0330

    Section 7 - Generic Repository

---
+++ WhiteLagoon.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using WhiteLagoon.Application.Common.Interfaces;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
