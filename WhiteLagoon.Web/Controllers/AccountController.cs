33b30624680518016d5c085d719b0f11b34b7ba6
Hessamsd <hessamsadeghi73@gmail.com>
2024-10-29 10:27:55 +0330

    Section 11 - Login Get Action

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Web.ViewModels;
 
 namespace WhiteLagoon.Web.Controllers
 {
@@ -15,18 +16,26 @@ namespace WhiteLagoon.Web.Controllers
 
 
         public AccountController(IUnitOfWork unitOfWork
-            , UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+            , UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager
+            , RoleManager<IdentityRole> roleManager)
         {
             _unitOfWork = unitOfWork;
             _userManager = userManager;
             _signInManager = signInManager;
-            _roleManager = roleManager;     
+            _roleManager = roleManager;
         }
 
 
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl = null)
         {
-            return View();
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
         }
 
         public IActionResult Register()
