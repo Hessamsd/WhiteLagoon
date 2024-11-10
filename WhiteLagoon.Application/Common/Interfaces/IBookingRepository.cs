f31e634e39bfe25c0efe8031914045544fe0299d
hessammayan <hasansadeghi.pdev@gmail.com>
2024-11-10 11:11:51 +0330

    Section 13 - More Methods in Booking Repository

--- WhiteLagoon.Application/Common/Interfaces/IBookingRepository.cs
+++ WhiteLagoon.Application/Common/Interfaces/IBookingRepository.cs
@@ -5,5 +5,7 @@ namespace WhiteLagoon.Application.Common.Interfaces
     public interface IBookingRepository : IRepository<Booking>
     {
         void Uppdate(Booking booking);
+        void UpdateStatus(int bookingId , string bookingStatus);
+        void UpdateStripPaymentID(int bookingId, string sessionId, string paymentIntentId);
     }
 }
